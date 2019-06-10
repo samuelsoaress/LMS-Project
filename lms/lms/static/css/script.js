867ce7384b3693f25649233cda191a6eb41e7870
Samuel Soares <42000522+getsam@users.noreply.github.com>
2019-06-10 14:39:03 -0300

    db.sqlite3

---
+++ lms/lms/static/css/script.js
@@ -0,0 +1,16 @@
+function enviar(){
+    var nome = document.getElementById("nome");
+    var data = document.getElementById("data");
+    var plano = document.getElementById("plano");
+    var carga = document.getElementById("carga");
+    var competencias = document.getElementById("competencias");
+    var habilidades = document.getElementById("habilidades");
+    var emenda = document.getElementById("emenda");
+    var programatico = document.getElementById("programatico");
+    var bibliografia = document.getElementById("bibliografia");
+    var biblioteca = document.getElementById("biblioteca"); 
+    var pratico = document.getElementById("pratico");
+    var teorico = document.getElementById("teorico");
+    var coordenador = document.getElementById("coordenador");
+
+    
---
+++ lms/lms/templates/README.md
@@ -0,0 +1,1 @@
+# teste-web-com-django
